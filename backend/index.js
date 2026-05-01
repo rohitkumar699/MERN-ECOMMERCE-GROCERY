d32e7ad1b2ade021b3f94d2f69cae8d4b8ce8f19
Rohit Kumar <rohit.kumar@hithaldia.ac.in>
2026-05-01 14:20:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,26 @@ import categoryRouter from "./routes/category.routes.js";
 import productRouter from "./routes/product.routes.js";
 import addressRouter from "./routes/address.routes.js";
 import orderRouter from "./routes/order.routes.js";
+
 const app = express();
 
 connectDB();
+
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://wonderful-cliff-0c9812800.7.azurestaticapps.net"
+  ],
+  credentials: true
+}));
 app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRouter);
 app.use("/api/admin", adminRouter);
@@ -27,6 +37,7 @@ app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
 app.use("/api/address", addressRouter);
 app.use("/api/order", orderRouter);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
